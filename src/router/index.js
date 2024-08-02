9c72dea52f96a4985fcdb2765b5ef4bee20273b2
S7thx <136721499+S7thx@users.noreply.github.com>
2024-08-02 11:46:10 -0300

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import VideoComponent from '@/components/VideoComponent.vue'
+import ImagemComponent from '@/components/ImagemComponent.vue'
+import TabelaComponent from '@/components/tabelaComponent.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+     path: '/videos',
+     name: 'videos',
+     component: VideoComponent,
+    },
+    {
+     path: '/imagens',
+     name: 'imagens',
+     component: ImagemComponent,
+    },
+    {
+     path: '/tabela',
+     name: 'tabela',
+     component: TabelaComponent,
+    },
+  ]
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ImagemComponent from '@/components/ImagemComponent.vue';
+import TabelaComponent from '@/components/tabelaComponent.vue';
+import VideoComponent from '@/components/VideoComponent.vue';
+</script>
+
+<template>
+  <main>
+    <h1 style="text-align: center;">Bolsonaro 22</h1><br>
+    <h3>"Brasil acima de tudo, Deus acima de todos"<br>
+        "O soldado que vai à guerra e tem medo de morrer é um covarde".</h3>
+    <ImagemComponent/>
+    <TabelaComponent/>
+    <VideoComponent/>
+  </main>
+</template>
